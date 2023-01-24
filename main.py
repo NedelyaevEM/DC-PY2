515f6914814c15f4b7dedc289078102e9ba352e1
NedelyaevEM <115372102+NedelyaevEM@users.noreply.github.com>
2023-01-24 14:44:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class Library:
 
 
     def get_next_book_id(self):
-        if self.books == []:
+        if not self.books:
             return 1
         else:
             return self.books[-1].id_ + 1
